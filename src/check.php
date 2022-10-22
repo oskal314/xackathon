2df1e35553ad577f17b312b19b869195ffb91d5f
oskal314 <artem-osipovartem3002@mail.ru>
2022-10-22 12:39:35 +0300

    Проверка формы регистрации и авторизации

--- src/check.php
+++ src/check.php
@@ -1,33 +1,39 @@
-<?php 
+<?php
 
-$name = filter_var(trim($_POST['name']),
-FILTER_SANITIZE_STRING);
-$login = filter_var(trim($_POST['login']),
-FILTER_SANITIZE_STRING);
-$pass = filter_var(trim($_POST['pass']),
-FILTER_SANITIZE_STRING);
-$role = filter_var(trim($_POST['role']),
-FILTER_SANITIZE_STRING);
+$name = filter_var(
+    trim($_POST['name']),
+    FILTER_SANITIZE_STRING
+);
+$login = filter_var(
+    trim($_POST['login']),
+    FILTER_SANITIZE_STRING
+);
+$pass = filter_var(
+    trim($_POST['pass']),
+    FILTER_SANITIZE_STRING
+);
+$role = filter_var(
+    trim($_POST['role']),
+    FILTER_SANITIZE_STRING
+);
 
-if(mb_strlen($login)<5 || mb_strlen($login)>90) {
+if (mb_strlen($login) < 5 || mb_strlen($login) > 90) {
     echo "Недопустимая длина логина";
     exit();
-} else if (mb_strlen($name)<10 || mb_strlen($name)>50) {
+} else if (mb_strlen($name) < 10 || mb_strlen($name) > 50) {
     echo "Недопустимая длина ФИО";
     exit();
-}else if (mb_strlen($pass)<2 || mb_strlen($pass)>6) {
+} else if (mb_strlen($pass) < 2 || mb_strlen($pass) > 6) {
     echo "Недопустимая длина пароля (от 2 до 6 символов)";
     exit();
 }
 
-$pass=md5($pass."fghsgfsuh4321");
+$pass = md5($pass . "fghsgfsuh4321");
 
-$mysql=new mysqli('localhost','root','root','webpractik');
+$mysql = new mysqli('localhost', 'root', '', 'webpractik');
 $mysql->query("INSERT INTO `users`(`name`,`login`,`pass`,`role`)
 VALUES('$name','$login','$pass','$role')");
 
 $mysql->close();
 
-header('Location: /');
-
-?>
+header('Location: index.html');
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $name = $_GET['name'];
 $phone = $_GET['phone'];
@@ -14,10 +14,10 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'artemosipovartem12@gmail.com';                 // Наш логин
-$mail->Password = 'osipov2003';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
- 
+
 $mail->setFrom('artemosipovartem12@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('artemisosipov11@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
@@ -35,10 +35,8 @@ $mail->Body    = '
 	Номер телефона: ' . $phone . '<br>
 	E-mail: ' . $email . '';
 
-if(!$mail->send()) {
-    return false;
+if (!$mail->send()) {
+	return false;
 } else {
-    return true;
+	return true;
 }
-
-?>
